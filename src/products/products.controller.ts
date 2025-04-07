2a82913022712884c12a8a1eefab06a38daeca1a
Pamela Pogo <pme.pogo@yavirac.edu.ec>
2025-04-06 19:05:37 -0500

    Clase 4

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -7,21 +7,47 @@ export class ProductsController {
         return "Estamos en produccion"
     }
 //Recibir un pparametro en la URL
-    //@Get(':id')
-   // find(@Param() params) {
-    //    return `Estas consultando el producto ${params.id}`
-   //}
+//  @Get(':id')
+//  find(@Param() params) {
+//    return `Estas consultando el producto ${params.id}`
+//  }
 
 //Recibir varios parametros en la URL tipados y desagregados
-    @Get(':id/:size')
-    findWithSize (@Param('id') id:number, @Param('size') size: string){
-        return `productos con id: ${params.id} ////// size: ${params.size}`
-}
+//    @Get(':id/:size')
+//    findWithSize (@Param() params){
+//        return `productos con id: ${params.id}, tiene el tamaño size: ${params.size}`
+// }
 
 //Desestructurar parametros de URL
-    @Get(':id')
-    find(@Param('id') is:number){
-        return `productos con id:`
+//    @Get(':id')
+//    find(@Param('id') id:number){
+//       return `productos con id: ${id}`
+//   }
+
+//Destruccion de 2 parametros
+    @Get(':id/:size')
+    findWithSize(@Param ('id') id: number, @Param('size') size: string) {
+        return `Detalle de producto ${id}, en tamaño ${size}`
     }
 
+//Definir una ruta POST
+//    @Post()
+//    createProduct(){
+//        return 'Estamos atendiendo una solicitud  de tipo Post'
+//    }
+
+//Recibir datos de POST
+//    @Post()
+//    createProduct(@Body() body){
+//        return `Creo un producto ${body.name} con descripción ${body.description}`
+//    }
+
+//Recibir datos Post del body por su nombre
+@Post()
+createProduct(
+  @Body('name') name: string, 
+  @Body('description') description: string
+) {
+  return `Creo el producto ${name} con descripción ${description}.`;
+}
 }
